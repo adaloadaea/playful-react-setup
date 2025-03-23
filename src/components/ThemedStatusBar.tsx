41f9195e90d6310a25eaefc59f81a6548e0320c2
adaloadaea <adalo@talelgym.tn>
2025-03-23 11:44:27 +0100

    dee

--- src/components/ThemedStatusBar.tsx
+++
@@ -1,9 +0,0 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { useTheme } from '../contexts/ThemeContext';
-
-export default function ThemedStatusBar() {
-  const { theme } = useTheme();
-  
-  return <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />;
-}
--- src/hooks/useThemeColors.ts
+++
@@ -1,8 +0,0 @@
-
-import { useTheme } from '../contexts/ThemeContext';
-import { getThemeColors } from '../styles/theme';
-
-export const useThemeColors = () => {
-  const { theme } = useTheme();
-  return getThemeColors(theme);
-};
